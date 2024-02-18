0155a5191d0955a4ba59922a106b14d60487c531
root <priyankaguptasp123@gmail.com>
2024-02-18 04:15:42 +0000

    first

---
+++ src/Components/HistoryItems/index.js
@@ -0,0 +1,7 @@
+const HistoryItems = props => {
+  const {historyItems} = props
+
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItems
+  return <li>h1</li>
+}
+export default HistoryItems
